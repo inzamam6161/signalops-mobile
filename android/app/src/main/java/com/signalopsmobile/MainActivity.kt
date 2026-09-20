e5481782d1ce62a97405ce4b8210a14f6f05cb37
Inzamamul Haque <inzamamulhaque@MacBook-Pro.local>
2026-09-20 14:33:34 +0400

    Upgrade SignalOps incident response workflows

--- android/app/src/main/java/com/signalopsmobile/MainActivity.kt
+++ android/app/src/main/java/com/signalopsmobile/MainActivity.kt
@@ -1,32 +1,33 @@
 package com.signalopsmobile
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import android.os.Bundle
 import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 
 class MainActivity : ReactActivity() {
 
-  supportFragmentManager.fragmentFactory =
-        RNScreensFragmentFactory()
-        super.onCreate(savedInstanceState)
-  }
-
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript.
    */
   override fun getMainComponentName(): String = "SignalOpsMobile"
 
   /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   * Uses React Native's default activity delegate with New Architecture support.
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    DefaultReactActivityDelegate(
+      this,
+      mainComponentName,
+      fabricEnabled,
+    )
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory =
+      RNScreensFragmentFactory()
 
+    super.onCreate(savedInstanceState)
+  }
 }
